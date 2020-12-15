98962027d9008ac87a49723825d798512888149d
Vedhak6 <Vedhak6>
2020-12-15 20:49:30 +0530

    12 dec assignment

---
+++ A.java
@@ -0,0 +1,35 @@
+package com.xworkz.alphabetProblem;
+
+public class A {
+
+	public static void main(String[] args) {
+		
+				int num =4;
+				for(int i =0;i<=num ;i++) {
+					for(int j=0;j<=num;j++) {
+						 if( j==0 && i>1 ||i==2 || j==4 && i>1 || i+j==2 || i+j==4 && i<j && i!=0 )
+							 System.out.print(" * ");
+						 else
+							 System.out.print("   ");
+
+					}
+					System.out.println();
+				
+
+			}
+
+		}
+
+		}
+		
+		
+		
+		
+		
+		
+		
+
+
+	
+
+
---
+++ B.java
@@ -0,0 +1,24 @@
+package com.xworkz.alphabetProblem;
+
+public class B {
+
+	public static void main(String[] args) {
+		
+		int num =4;
+		for(int i =0;i<=num ;i++) {
+			for(int j=0;j<=num;j++) {
+				 if(j==0  || i==2 && j!=4 || i==0 && j!=4 || i==4 && j!=4 || i+j>num && i<j && i!=2 )
+					 System.out.print("*");
+				 else
+					 System.out.print(" ");
+
+			}
+			System.out.println();
+		
+
+	}
+
+		
+	}
+
+}
